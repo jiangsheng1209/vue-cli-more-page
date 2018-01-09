0da3364f03589177a32e5509d3701a5cea734d71
jiangsheng1209 <522310244@qq.com>
2018-01-09 17:45:49 +0800

    no change

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -7,13 +7,15 @@ var HtmlWebpackPlugin = require('html-webpack-plugin')
 var FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 
 // add hot-reload related code to entry chunks
-Object.keys(baseWebpackConfig.entry).forEach(function (name) {
+Object.keys(baseWebpackConfig.entry).forEach(function(name) {
   baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(baseWebpackConfig.entry[name])
 })
 
 module.exports = merge(baseWebpackConfig, {
   module: {
-    rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap })
+    rules: utils.styleLoaders({
+      sourceMap: config.dev.cssSourceMap
+    })
   },
   // cheap-module-eval-source-map is faster for development
   devtool: '#cheap-module-eval-source-map',
@@ -25,24 +27,24 @@ module.exports = merge(baseWebpackConfig, {
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
     // https://github.com/ampedandwired/html-webpack-plugin
-   /*  new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'index.html',
-      inject: true
-    }), */
+    /*  new HtmlWebpackPlugin({
+       filename: 'index.html',
+       template: 'index.html',
+       inject: true
+     }), */
     new FriendlyErrorsPlugin()
   ]
 })
 
 
-var pages =  utils.getMultiEntry('./src/'+config.moduleName+'/**/**/*.html');
+var pages = utils.getMultiEntry('./src/' + config.moduleName + '/**/**/*.html');
 for (var pathname in pages) {
   // 配置生成的html文件，定义路径等
   var conf = {
     filename: pathname + '.html',
     template: pages[pathname], // 模板路径
     chunks: [pathname, 'vendors', 'manifest'], // 每个html引用的js模块
-    inject: true              // js插入位置
+    inject: true // js插入位置
   };
   // 需要生成几个html文件，就配置几个HtmlWebpackPlugin对象
   module.exports.plugins.push(new HtmlWebpackPlugin(conf));
